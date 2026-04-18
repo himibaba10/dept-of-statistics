f7e3faaba86febe5accb637f5a39ea0e65ec8917
Ferdous Ahmed <himibaba10@gmail.com>
2026-04-19 02:53:02 +0600

    feat: initial commit

---
+++ src/types/index.ts
@@ -0,0 +1,34 @@
+export type Role = 'student' | 'teacher' | 'official';
+
+export interface UserBase {
+  _id: string;
+  name: string;
+  email: string;
+  phone: string;
+  address: string;
+  bloodGroup: string;
+  imageUrl: string;
+  role: Role;
+}
+
+export interface Student extends UserBase {
+  role: 'student';
+  studentId: string;
+  session: string; 
+  isCR: boolean;
+}
+
+export interface Teacher extends UserBase {
+  role: 'teacher';
+  designation: string;
+  galleryUrls: string[];
+  hasAdminAccess: boolean; 
+}
+
+export interface Course {
+  _id: string;
+  code: string; 
+  title: string; 
+  syllabusImageUrl: string;
+  instructorId: string; 
+}
